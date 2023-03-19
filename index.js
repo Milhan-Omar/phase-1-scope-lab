503c88177031f7cd4da54d8979054373e8e31b66
Milhan Osman <milhanomarosman@gmail.com>
2023-03-19 19:34:53 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Write your solution in this file!
+var customerName = 'bob';
+// console.log('====================================');
+// console.log(customerName);
+// console.log('====================================');
+function upperCaseCustomerName(){
+  customerName = customerName.toUpperCase()
+ return customerName ;
+}
+// upperCaseCustomerName()
+// customerName(upperCaseCustomerName());
+// console.log('====================================');
+// console.log(upperCaseCustomerName());
+// console.log('====================================');
+// let bestCustomer = "no bob"
+function setBestCustomer(){
+ return bestCustomer = "not bob";
+}   
+// setBestCustomer();    
+// console.log('====================================');
+// console.log(window.bestCustomer())
+// console.log('====================================');
+function overwriteBestCustomer(){
+  return bestCustomer = 'maybe bob';
+}
+function changeLeastFavoriteCustomer(){
+  throw 'Assignment to constant variable.';
+}
+
